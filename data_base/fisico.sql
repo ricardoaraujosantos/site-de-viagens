29686dcacd0832908e343cd357968d5543badd22
Ricardo <ricardoaraujo3713@gmail.com>
2022-08-23 17:49:54 -0300

    Pagina logar, cadastro usuario data_base  modelo conceitual, logico, fisico e UML

--- data_base/fisico.sql
+++ data_base/fisico.sql
@@ -1,55 +1,60 @@
 create database agencia_viajar;
 use agencia_viajar;
 
+CREATE TABLE permissao (
+id_permissao INTEGER PRIMARY KEY auto_increment,
+tipo VARCHAR(20)
+);
+
 CREATE TABLE usuario (
-id_usuario INT PRIMARY KEY auto_increment,
+id_usuario INTEGER PRIMARY KEY auto_increment,
 nome VARCHAR(50),
 senha VARCHAR(50),
-endereco VARCHAR(50)
+email VARCHAR(50),
+endereco VARCHAR(50),
+id_permissao INTEGER,
+FOREIGN KEY(id_permissao) REFERENCES permissao (id_permissao)
 );
 
 CREATE TABLE compra (
-id_compra INT PRIMARY KEY auto_increment,
-data_compra DATE,
+id_compra INTEGER PRIMARY KEY auto_increment,
 valor_compra DECIMAL(6),
-id_usuario INT,
+data_compra DATE,
+id_usuario INTEGER,
 FOREIGN KEY(id_usuario) REFERENCES usuario (id_usuario)
 );
 
 CREATE TABLE passagem (
-id_passagem INT PRIMARY KEY auto_increment,
-destino VARCHAR(50),
-nome_empresa VARCHAR(50),
+id_passagem INTEGER PRIMARY KEY auto_increment,
+empresa VARCHAR(50),
+embarque VARCHAR(50),
+desembarque VARCHAR(50),
 valor_passagem DECIMAL(6),
-data_passagem DATE,
-embarque VARCHAR(50)
+data_passagem DATE
 );
 
 CREATE TABLE hospedagem (
-id_hospedagem INT PRIMARY KEY auto_increment,
-nome_empresa VARCHAR(50),
+id_hospedagem INTEGER PRIMARY KEY auto_increment,
+empresa VARCHAR(50),
 endereco VARCHAR(50),
 valor_diaria DECIMAL(6),
-tipo_diaria varchar(30),
 data_hospedagem DATE
 );
 
-CREATE TABLE item_hospedagem (
-qtd_itens INT,
-valor_itens DECIMAL(6),
-id_hospedagem iNT,
-id_compra INT,
-FOREIGN KEY(id_hospedagem) REFERENCES hospedagem (id_hospedagem),
-FOREIGN KEY(id_compra) REFERENCES compra (id_compra)
-);
-
 CREATE TABLE item_passagem (
-qtd_itens INT,
-valor_itens DECIMAL(6),
-id_passagem INT,
-id_compra INT,
+qtd_item DECIMAL(6),
+valor_item DECIMAL(6),
+id_passagem INTEGER,
+id_compra INTEGER,
 FOREIGN KEY(id_passagem) REFERENCES passagem (id_passagem),
 FOREIGN KEY(id_compra) REFERENCES compra (id_compra)
 );
 
-
+CREATE TABLE item_hospedagem (
+valor_item DECIMAL(6),
+qtd_item DECIMAL(6),
+id_hospedagem INTEGER,
+id_compra INTEGER,
+FOREIGN KEY(id_hospedagem) REFERENCES hospedagem (id_hospedagem),
+FOREIGN KEY(id_compra) REFERENCES compra (id_compra)
+);
